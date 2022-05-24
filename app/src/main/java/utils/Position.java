c142acbd0d42050f8c37aa32df4b0a4907f1729a
durki <michal@durkalec.com>
2022-05-24 17:16:01 +0200

    completed position class

--- app/src/main/java/utils/Position.java
+++ app/src/main/java/utils/Position.java
@@ -1,5 +1,8 @@
 package utils;
 
+// Contains information about position on 2D plane
+// Should not be mutated
+
 public class Position {
     public Integer x;
     public Integer y;
@@ -7,6 +10,14 @@ public class Position {
     public Position(Integer x, Integer y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
     }
 
 }
