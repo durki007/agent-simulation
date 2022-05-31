92f1940393253c2803a8ec4a70d0ce8f8b439032
xdx1354 <stanislaw1354@gmail.com>
2022-05-31 20:11:25 +0200

    Ratio input from CLI added

--- app/src/main/java/utils/RatioUtils.java
+++ app/src/main/java/utils/RatioUtils.java
@@ -1,13 +1,23 @@
 package utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RatioUtils {
-    public static ArrayList<Integer> getRatio() {
-        ArrayList<Integer> ratio = new ArrayList<Integer>();
-        ratio.add(1);
-        ratio.add(1);
-        ratio.add(1);
+    public static ArrayList<Integer> getRatio(String newRatio) {
+
+        //Parsing string to Arraylist of Integers
+        String cutRatio = newRatio.substring(1,newRatio.length()-2); //Cuts the ( ) brackets
+        String []elements = cutRatio.split(","); //converts to array of strings
+        List<String> elementsAsList = Arrays.asList(elements); //converts to List
+        ArrayList<String> listOfString = new ArrayList<String>(elementsAsList); //converts to ArrayList
+        ArrayList<Integer> ratio = new ArrayList<Integer>(); //creates ArrayList of Integers
+        //adds int value of strings from ArrayList listOfStrings to ArrayList ratio
+        for(int i = 0; i < listOfString.size(); i++) {
+            ratio.add(Integer.parseInt(listOfString.get(i)));
+        }
+
         return ratio;
     }
 }
